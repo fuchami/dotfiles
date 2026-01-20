a21e594ca8dc71617128aaabe909d2cb32dd8ac8
fuchami <fuchami23@icloud.com>
2026-01-20 23:20:45 +0900

    add resize&centering

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -2,12 +2,25 @@
 -- $ defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.config/hammerspoon/init.lua"
 
 hs.hotkey.bind({ "option" }, "space", function()
-  local appName = "WezTerm"
-  local app = hs.application.get(appName)
+	local appName = "WezTerm"
+	local app = hs.application.get(appName)
 
-  if app == nil or app:isHidden() then
-    hs.application.launchOrFocus(appName)
-  else
-    app:hide()
-  end
+	if app == nil or app:isHidden() then
+		hs.application.launchOrFocus(appName)
+
+	-- resize & centering
+	-- win = app:mainWindow()
+	-- local screen = hs.mouse.getCurrentScreen()
+	-- local max = screen:frame()
+	--
+	-- local width = max.w * 0.9
+	-- local height = max.h * 0.9
+	--
+	-- local x = max.x + (max.w - width) / 2
+	-- local y = max.y + (max.h - height) / 2
+	--
+	-- win:setFrame({ x = x, y = y, w = width, h = height }, 0.05)
+	else
+		app:hide()
+	end
 end)
