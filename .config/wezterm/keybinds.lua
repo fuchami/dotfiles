cf5ab11cf94e67867c32cb0b82c929fa906ff2d3
fuchami <fuchami23@icloud.com>
2026-01-21 00:07:58 +0900

    apply smart-splits.nvim

--- .config/wezterm/keybinds.lua
+++ .config/wezterm/keybinds.lua
@@ -1,6 +1,40 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
 
+-- if you are *NOT* lazy-loading smart-splits.nvim (recommended)
+local function is_vim(pane)
+	-- this is set by the plugin, and unset on ExitPre in Neovim
+	return pane:get_user_vars().IS_NVIM == "true"
+end
+
+local direction_keys = {
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
+}
+
+local function split_nav(resize_or_move, key)
+	return {
+		key = key,
+		mods = resize_or_move == "resize" and "CTRL|META" or "CTRL",
+		action = wezterm.action_callback(function(win, pane)
+			if is_vim(pane) then
+				-- pass the keys through to vim/nvim
+				win:perform_action({
+					SendKey = { key = key, mods = resize_or_move == "resize" and "CTRL|META" or "CTRL" },
+				}, pane)
+			else
+				if resize_or_move == "resize" then
+					win:perform_action({ AdjustPaneSize = { direction_keys[key], 2 } }, pane)
+				else
+					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+				end
+			end
+		end),
+	}
+end
+
 return {
 	-- tmux-like keybindings
 	keys = {
@@ -16,14 +50,17 @@ return {
 		{ key = "|", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
 		{ key = "-", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
 
-		-- Pane移動 leader + hjkl
-		{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
-		{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
-		{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
-		{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+		-- MovePane (ctrl + hjkl)
+		split_nav("move", "h"),
+		split_nav("move", "j"),
+		split_nav("move", "k"),
+		split_nav("move", "l"),
 
-		-- Active Resize Keytable
-		{ key = "s", mods = "LEADER", action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }) },
+		-- PaneResize (alt-ctrl + hjkl)
+		split_nav("resize", "h"),
+		split_nav("resize", "j"),
+		split_nav("resize", "k"),
+		split_nav("resize", "l"),
 
 		-- Pane Zoom
 		{ key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
@@ -31,15 +68,5 @@ return {
 		-- CopyMode
 		{ key = "[", mods = "LEADER", action = act.ActivateCopyMode },
 	},
-	key_tables = {
-		-- PaneResize leader + s
-		resize_pane = {
-			{ key = "h", action = act.AdjustPaneSize({ "Left", 1 }) },
-			{ key = "l", action = act.AdjustPaneSize({ "Right", 1 }) },
-			{ key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
-			{ key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
-			-- Cancel the mode by pressing escape
-			{ key = "Enter", action = "PopKeyTable" },
-		},
-	},
+	key_tables = {},
 }
