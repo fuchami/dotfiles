a64534bf27704a8a9c0bb84341571d2633a45870
fuchami <fuchami23@icloud.com>
2026-01-20 23:21:30 +0900

    update tabline appearance

--- .config/wezterm/tabline.lua
+++ .config/wezterm/tabline.lua
@@ -6,25 +6,29 @@ return {
 		theme = "catppuccin-mocha",
 		theme_overrides = {
 			tab = {
-				-- active = { fg = "#181818", bg = "#9F7AEA" },
+				active = { fg = "#111111", bg = "#c4a000" },
 			},
 		},
 		section_separators = {
-			left = wezterm.nerdfonts.ple_upper_left_triangle,
-			right = wezterm.nerdfonts.ple_upper_right_triangle,
+			-- left = wezterm.nerdfonts.ple_upper_left_triangle,
+			-- right = wezterm.nerdfonts.ple_upper_right_triangle,
+			left = "",
+			right = "",
 		},
 		component_separators = {
-			left = wezterm.nerdfonts.ple_forwardslash_separator,
-			right = wezterm.nerdfonts.ple_forwardslash_separator,
+			left = wezterm.nerdfonts.pl_left_soft_divider,
+			right = wezterm.nerdfonts.pl_left_soft_divider,
 		},
 		tab_separators = {
-			left = wezterm.nerdfonts.ple_lower_left_triangle,
-			right = wezterm.nerdfonts.ple_lower_right_triangle,
+			-- left = wezterm.nerdfonts.ple_lower_left_triangle,
+			-- right = wezterm.nerdfonts.ple_lower_right_triangle,
+			left = "",
+			right = "",
 		},
 	},
 	sections = {
-		tabline_a = { " 󰰮 " },
-		tabline_b = { "mode" },
+		tabline_a = { " 󰰮 ", "mode" },
+		tabline_b = {},
 		tabline_c = {},
 		tab_active = {
 			{ "index", padding = 1 },
