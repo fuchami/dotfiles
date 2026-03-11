a72fb4df3b729b9bdf963152e24e68e328b23ebf
fuchami <fuchami23@icloud.com>
2026-03-11 13:17:27 +0900

    add ghq-fzf

--- .config/zsh/functions/function.zsh
+++ .config/zsh/functions/function.zsh
@@ -8,6 +8,18 @@ function y() {
 	rm -f -- "$tmp"
 }
 
+# ghq + fzf wrapper
+function ghq-fzf() {
+  local src=$(ghq list | fzf --preview "bat --color=always --style=header,grid --line-range :80 $(ghq root)/{}/README.*")
+  if [ -n "$src" ]; then
+    BUFFER="cd $(ghq root)/$src"
+    zle accept-line
+  fi
+  zle -R -c
+}
+zle -N ghq-fzf
+bindkey '^g' ghq-fzf
+
 # zoxide wrapper
 function zn() {
   z "$@" && nvim
